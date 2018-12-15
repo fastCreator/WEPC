4d579fe76de44d2fb77ad9e1ae97fcde0b4f2eab
Your Name <you@example.com>
2018-12-15 20:06:20 +0800

    修复报错

--- src/nav.js
+++ src/nav.js
@@ -6,11 +6,13 @@ components.keys().sort((a, b) => {
 }).forEach((item, i) => {
   let path = item.split('/')
   let pos = navs
-  for (let i = 0, len = path.length - 3; i < len; i++) {
-    pos = pos[pos.length - 1].children
+  if (pos.length - 1 > 0) {
+    for (let i = 0, len = path.length - 3; i < len; i++) {
+      pos = pos[pos.length - 1].children
+    }
   }
   let nav = components(item).nav
-  pos.push(Object.assign({children: [], name: item.replace('/config.js', '').replace('./', '/')}, nav))
+  pos.push(Object.assign({ children: [], name: item.replace('/config.js', '').replace('./', '/') }, nav))
 })
 
 export default navs
